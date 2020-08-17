fbf12daa08a69f5486d5d1b328326539acb22c8a
Balazs M. Fekete <bfekete@ccny.cuny.edu>
2020-08-17 06:44:00 -0400

    Revising to work with clang.

--- include/CSTsurveying.h
+++ include/CSTsurveying.h
@@ -2,5 +2,4 @@ typedef struct CSTsurvPoint_s {
     char Name[11];
     double X, Y, Z;
     char Pcode[11];
-    char Padding[18];
 } CSTsurvPoint_t;
--- src/CSTsurveying.c
+++ src/CSTsurveying.c
@@ -1,45 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 #include <CSTsurveying.h>
-
-void CSTbyteOrderSwapWord(void *data) {
-    char ch;
-    ch = ((char *) data)[0];
-    ((char *) data)[0] = ((char *) data)[3];
-    ((char *) data)[3] = ch;
-    ch = ((char *) data)[1];
-    ((char *) data)[1] = ((char *) data)[2];
-    ((char *) data)[2] = ch;
-}
-
-void CSTbyteOrderSwapLongWord(void *data) {
-    char ch;
-
-    ch = ((char *) data)[0];
-    ((char *) data)[0] = ((char *) data)[7];
-    ((char *) data)[7] = ch;
-    ch = ((char *) data)[1];
-    ((char *) data)[1] = ((char *) data)[6];
-    ((char *) data)[6] = ch;
-    ch = ((char *) data)[2];
-    ((char *) data)[2] = ((char *) data)[5];
-    ((char *) data)[5] = ch;
-    ch = ((char *) data)[3];
-    ((char *) data)[3] = ((char *) data)[4];
-    ((char *) data)[4] = ch;
-}
 
 int main(int argv, char *argc[]) {
     int i;
+    char buffer[64];
     CSTsurvPoint_t survPoint;
 
-    printf("%d\n", (int) sizeof(CSTsurvPoint_t));
-    while (fread(&survPoint, sizeof(CSTsurvPoint_t), 1, stdin) == 1) {
-        for (i = 0; i < 10; ++i) survPoint.Name[i] = survPoint.Name[i + 1];
+    printf("\"PCode\"\t\"Name\"\t\"Xcoord\"\t\"Ycoord\"\t\"Elevation\"\n");
+    while (fread(buffer, 64, 1, stdin) == 1) {
+        for (i = 0; i < 10 && buffer[i + 1] != ' '; ++i) survPoint.Name[i] = buffer[i + 1];
         survPoint.Name[i] = '\0';
-        for (i = 0; i < 10; ++i) survPoint.Pcode[i] = survPoint.Pcode[i + 1];
+        for (i = 0; i < 10 && buffer[i + 36] != ' '; ++i) survPoint.Pcode[i] = buffer[i + 36];
         survPoint.Pcode[i] = '\0';
-        printf("P#: %s X: %lf Y: %lf Z: %lf Pcode: %s\n", survPoint.Name, (double) survPoint.X, (double) survPoint.Y,
-               (double) survPoint.Z, survPoint.Pcode);
+        memcpy(&(survPoint.X),buffer + 11,sizeof(double));
+        memcpy(&(survPoint.Y),buffer + 19,sizeof(double));
+        memcpy(&(survPoint.Z),buffer + 27,sizeof(double));
+        printf("\"%s\"\t\"%s\"\t%lf\t%lf\t%lf\n", survPoint.Pcode,survPoint.Name,survPoint.X,survPoint.Y,survPoint.Z);
     }
     return (0);
 }
